83093956e6269d3bd0f1cd7c46cc4e82f41b9a75
kimberli <kimberlizhong@gmail.com>
2018-05-20 14:52:13 -0400

    add figures

---
+++ figures/convert.sh
@@ -0,0 +1,7 @@
+colors=("#D11B1B" "#247BA0" "#3E9361" "#955DA0")
+i=0
+for f in E_*.svg; do
+    echo ${colors[$((i%4))]};
+    svgexport $f $(basename $f .svg).png "svg{} path{stroke-width:0.05px; stroke: ${colors[$((i%4))]}}"
+    i=$((i+1))
+done
